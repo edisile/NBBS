1800f39c911af86876a700d0f77d181d72e6d71f
edisile <mantapeppe@gmail.com>
2021-05-06 15:55:13 +0200

    Added $(seq ...) handling to the results parser

--- scripts/results_to_csv.py
+++ scripts/results_to_csv.py
@@ -16,6 +16,15 @@ if len(sys.argv) > 1:
 config = imp.load_source("bash_module", "./config.sh")
 directory = f"results_{config.NUM_LEVELS}_{config.MAX}_{config.MIN}"
 
+# check whether RUN_list was specified as "$(seq ...)"
+x = re.compile(r"\$\(seq (?P<num>[0-9]*)\)").findall(config.RUN_list)
+if len(x) == 1:
+    # yep, it was a seq command expansion
+    config.RUN_list = ""
+    for i in range(1, int(x[0])):
+        config.RUN_list += f"{i} "
+    config.RUN_list += f"{int(x[0])}"
+
 print("Test config:")
 print(f"\tallocators: {config.ALLOC_list}")
 print(f"\tsizes: {config.SIZE_list}")
@@ -42,7 +51,7 @@ print(f"\tlevels: {config.NUM_LEVELS}")
 print(f"\nParsing files in {directory}")
 
 # regex match the timer line in the result files
-parser = re.compile("Timer\s+\(clocks\): [0-9]*")
+parser = re.compile(r"Timer\s+\(clocks\): (?P<num>[0-9]*)")
 
 file_list = []
 for test in tests:
@@ -50,11 +59,12 @@ for test in tests:
         for size in sizes:
             for th in threads:
                 for run in runs:
-                    #file_list.append(f"{test}-{alloc}-sz{size}-TH{th}-R{run}")
                     file_list.append((test, alloc, size, th, run))
 
 for idx, filetup in enumerate(file_list, start = 1):
-    print(f"\rProgress: {idx}/{len(file_list)}", end="")
+    # print a simple progress indicator
+    comp = 100 * idx // (2 * len(file_list))
+    print(f"\r[{comp * '='}{(50-comp)*' '}] {idx}/{len(file_list)}", end="")
 
     test, alloc, size, th, run = filetup
     filename = f"{test}-{alloc}-sz{size}-TH{th}-R{run}"
@@ -68,14 +78,13 @@ for idx, filetup in enumerate(file_list, start = 1):
     try:
         with open(f"{directory}/{filename}", "r") as f:
             s = f.read()
-            match = parser.findall(s)
-            timer = match[0].split(" ")[-1] # get only the timer
+            timer = parser.findall(s)[0]
             data["Time"].append(timer)
     except:
         data["Time"].append("NaN")
         pass
 
-print(f"\nParsing completed, output to {output}")
-
 df = pd.DataFrame(data, columns = columns)
 df.to_csv(output, index=False)
+
+print(f"\nParsing completed, output written to {output}")
